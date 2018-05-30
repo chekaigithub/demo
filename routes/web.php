daf6382f29fde78170afd6cca6e73ba13694e5e9
chekaigithub <2485611545@qq.com>
2018-05-30 10:03:44 +0000

    4.0

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,12 @@
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the 'web' middleware group. Now create something great!
 |
 */
 
-Route::get("/","IndexController@home");
-Route::get("/help","IndexController@help");
-Route::get("/about","IndexController@about");
+Route::get('/','IndexController@home')->name('home');;
+Route::get('/help','IndexController@help')->name('help');
+Route::get('/about','IndexController@about')->name('about');
+
+Route::get('signup','UsersController@create')->name('signup');
