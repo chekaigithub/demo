135dde995f5b14ef1a719afbdc55f4cd7cdb8795
chekaigithub <2485611545@qq.com>
2018-06-01 16:47:51 +0000

    微博发布

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,20 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Requests;
+use App\Models\Status;
+use Auth;
+
 class IndexController extends Controller
 {
+
     public function home()
     {
-    	return view('index/home');
+        $feed_items = [];
+        if (Auth::check()) {
+            $feed_items = Auth::user()->feed()->paginate(30);
+        }
+
+        return view('static_pages/home', compact('feed_items'));
     }
 
     public function help()
     {
-    	return view('index/help');
+        return view('static_pages/help');
     }
 
     public function about()
     {
-    	return view('index/about');
+        return view('static_pages/about');
     }
 }
